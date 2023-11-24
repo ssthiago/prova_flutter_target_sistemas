a8928421d4182e88847d54c7aa670f9593a81ece
Thiago Sarmento Magalhaes <thiago.sarmento@gmail.com>
2023-11-24 10:17:38 -0300

    :recycle: refactor: injeção de dependencias refatoradas para a o arquivo /core/services/services.dart

--- lib/domian/usecases/authentication/login/login_usecase.dart
+++ lib/domian/usecases/authentication/login/login_usecase.dart
@@ -1,4 +1,6 @@
+import 'package:prova_flutter_target_sistemas/data/services/authenticated/i_user_session_manager.dart';
 import 'package:prova_flutter_target_sistemas/domian/entities/user.dart';
+import 'package:prova_flutter_target_sistemas/domian/entities/user_session.dart';
 import 'package:prova_flutter_target_sistemas/domian/repositories/i_authentication_repository.dart';
 import 'package:prova_flutter_target_sistemas/domian/result/result.dart';
 import 'package:prova_flutter_target_sistemas/domian/usecases/authentication/login/result/login_params.dart';
@@ -9,8 +11,9 @@ import 'result/login_success.dart';
 
 class LoginUseCase implements IUseCase<LoginParams, Result> {
   final IAuthenticationRepository repository;
+  final IUserSessionManager sessionManager;
 
-  LoginUseCase(this.repository);
+  const LoginUseCase(this.repository, this.sessionManager);
 
   @override
   Future<Result> call(LoginParams params) async {
@@ -19,6 +22,10 @@ class LoginUseCase implements IUseCase<LoginParams, Result> {
           user: User(username: params.username, password: params.password));
 
       if (authenticatedUser != null) {
+        await sessionManager.addSession(
+          authenticatedUser.id!,
+          UserSession(isAuthenticated: true, user: authenticatedUser),
+        );
         return LoginSuccess(authenticatedUser: authenticatedUser);
       } else {
         return LoginFailure('Username ou senha inválida');
--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:get_it/get_it.dart';
-import 'package:prova_flutter_target_sistemas/data/repositories/authentication_repository_impl.dart';
-import 'package:prova_flutter_target_sistemas/domian/usecases/authentication/login/login_usecase.dart';
-import 'package:prova_flutter_target_sistemas/presentation/login/login_store.dart';
 import 'package:prova_flutter_target_sistemas/routes/app_routes.dart';
 
-import 'data/datasource/remote/api/dio/dio_client_api.dart';
-import 'data/datasource/remote/api/i_client_api.dart';
-import 'data/datasource/remote/authentication_datasource_impl.dart';
-import 'data/datasource/remote/i_authentication_datasource.dart';
-import 'domian/repositories/i_authentication_repository.dart';
-import 'presentation/common_widgets/bottom_sheet/bottom_sheet_store.dart';
+import 'core/services/services.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
-  GetIt getIt = GetIt.I;
-
-  //registra e inicializa o DioCLientApi
-  GetIt.instance.registerLazySingleton<IClientApi>(() {
-    DioClientApi clientApi = DioClientApi();
-    clientApi.init();
-    return clientApi;
-  });
-
-  getIt.registerSingleton<BottomSheetStore>(BottomSheetStore());
-  getIt.registerSingleton<IAuthenticationDataSource>(
-      AuthenticationDataSourceImpl(clientApi: getIt.get<IClientApi>()));
-  getIt.registerSingleton<IAuthenticationRepository>(
-      AuthenticationRepositoryImpl(getIt.get<IAuthenticationDataSource>()));
-  getIt.registerSingleton<LoginUseCase>(LoginUseCase(getIt.get<IAuthenticationRepository>()));
-  getIt.registerSingleton<LoginStore>(LoginStore(
-      loginUseCase: getIt.get<LoginUseCase>(), bottomSheetStore: getIt.get<BottomSheetStore>()));
+  await Services.init();
 
   runApp(MyApp());
 }
